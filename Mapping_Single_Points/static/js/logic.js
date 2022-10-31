c8b5cd16cf66d60b91e162bedaf8a98523fda2b0
vneeraja <111020934+vneeraja@users.noreply.github.com>
2022-10-31 18:25:29 -0500

    Adding marker to the map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -10,6 +10,9 @@ let map = L.map("mapid", {
     zoom: 4
 });
 
+// Adding marker to the map for Los Angeles, California.
+var marker = L.marker([34.0522, -118.2437]).addTo(map);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
